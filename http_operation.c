5fccae834bfa4fdfedcf18aa073a8de6d6a06f8d
Steven HH Chen <s335276@gmail.com>
2021-01-30 01:21:26 +0800

    feat: parse text of HTTP GET body

--- http_operation.c
+++ http_operation.c
@@ -1,9 +1,42 @@
 #include "http_operation.h"
-#define TASK_URL "https://aiot-task-api.azurewebsites.net/api/GetACData?air_id=1&time=2020/01/21_00:00:00"
+#define TASK_URL "https://aiot-task-api.azurewebsites.net/api/GetACData?air_id=1&time=2020/01/01_00:00:00"
+
+struct string {
+    char* ptr;
+    size_t len;
+};
+
+void init_string(struct string* s) {
+    s->len = 0;
+    s->ptr = malloc(s->len + 1);
+    if (s->ptr == NULL) {
+        fprintf(stderr, "malloc() failed\n");
+        exit(EXIT_FAILURE);
+    }
+    s->ptr[0] = '\0';
+}
+
+size_t writefunc(void* ptr, size_t size, size_t nmemb, struct string* s)
+{
+    size_t new_len = s->len + size * nmemb;
+    s->ptr = realloc(s->ptr, new_len + 1);
+    if (s->ptr == NULL) {
+        fprintf(stderr, "realloc() failed\n");
+        exit(EXIT_FAILURE);
+    }
+    memcpy(s->ptr + s->len, ptr, size * nmemb);
+    s->ptr[new_len] = '\0';
+    s->len = new_len;
+
+    return size * nmemb;
+}
 
 int http_get_env_data() {
 	CURL* curl;
 	CURLcode res;
+
+    struct string s;
+    init_string(&s);
 
     int return_code = 0;
 
@@ -16,15 +49,20 @@ int http_get_env_data() {
         curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
 
         /* Perform the request, res will get the return code */
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunc);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
         res = curl_easy_perform(curl);
+        Log_Debug("%s\n", s.ptr);
+        free(s.ptr);
         /* Check for errors */
         if (res != CURLE_OK){
             fprintf(stderr, "curl_easy_perform() failed: %s\n",
                 curl_easy_strerror(res));
             return_code = -1;
         }   
-        else
-            Log_Debug("GET DATA OK!\n");
+        else {
+            Log_Debug("CURL GET DATA OK!\n");
+        }
 
         /* always cleanup */
         curl_easy_cleanup(curl);
